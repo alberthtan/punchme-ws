25c79761ac6d9db81d82ff2d6d537ca5ca46b068
Albert Tan <alberttan@Alberts-MacBook-Pro.local>
2023-04-01 16:36:47 -0400

    test

--- server.py
+++ server.py
@@ -10,42 +10,49 @@ RESTAURANTS = dict() # {restaurant_id: websocket, ...}
 CUSTOMERS = dict() # {customer_id: websocket, ...}
 
 async def handler(websocket, path):
-    print("handler")
     role = None
     try:
+        # Extract the access token from the query parameters of the websocket request
+        query_params = parse_qs(urlparse(path).query)
+        token = query_params.get("access_token", [None])[0]
+        if not token:
+            raise ValueError("Missing access token")
+
+        # Verify and decode the JWT
+        payload = jwt.decode(token, os.environ.get("SECRET_KEY_WS"), algorithms=['HS256'])
+        print("payload")
+        print(payload)
+        expiration_time = datetime.datetime.fromtimestamp(payload['exp'])
+
+        if expiration_time < datetime.datetime.now():
+            # Token has expired
+            raise ValueError("Token expired")
+
+        # Extract the user information from the payload
+        id = payload.get("id")
+        role = payload.get("role")
+        if id is None or role is None:
+            raise ValueError("Missing user information")
+
+        # Add the WebSocket to the appropriate dictionary
+        if role == "CUSTOMER":
+            print("connecting customer with id: " + str(id))
+            CUSTOMERS[id] = websocket
+        elif role == "RESTAURANT":
+            print("connecting restaurant with id: " + str(id))
+            RESTAURANTS[id] = websocket
+        else:
+            raise ValueError("Missing role")
+
         async for websocket_message in websocket:
             message = json.loads(websocket_message)
             print("message")
             print(message)
 
-            # Extract the access token from the query parameters of the websocket request
-            query_params = parse_qs(urlparse(path).query)
-            token = query_params.get("access_token", [None])[0]
-            if not token:
-                raise ValueError("Missing access token")
-
-            # Verify and decode the JWT
-            payload = jwt.decode(token, os.environ.get("SECRET_KEY_WS"), algorithms=['HS256'])
-            print("payload")
-            print(payload)
-            expiration_time = datetime.datetime.fromtimestamp(payload['exp'])
-
-            if expiration_time < datetime.datetime.now():
-                # Token has expired
-                raise ValueError("Token expired")
-
-            # Extract the user information from the payload
-            id = payload.get("id")
-            role = payload.get("role")
-            if id is None or role is None:
-                raise ValueError("Missing user information")
-
             if role == "CUSTOMER":
                 await handle_customer(websocket, id, message)
             elif role == "RESTAURANT":
                 await handle_restaurant(websocket, id, message)
-            else:
-                raise ValueError("Missing role")
 
     except Exception as e:
         # Handle authentication errors and other exceptions
@@ -66,10 +73,7 @@ async def handler(websocket, path):
 
 async def handle_customer(websocket, id, message):
     # Handle the websocket connection for a customer
-    print("connecting customer with id: " + str(id))
     print("message: " + str(message))
-    
-    CUSTOMERS[id] = websocket
 
     if "restaurant_id" in message:
         print("sending to restaurant " + str(message["restaurant_id"]))
@@ -85,21 +89,12 @@ async def handle_customer(websocket, id, message):
 
 async def handle_restaurant(websocket, id, message):
     # Handle the websocket connection for a restaurant
-    print("connecting restaurant with id: " + str(id))
     print("message: " + str(message))
-
-    RESTAURANTS[id] = websocket
 
     if "customer_id" in message:
         print("sending to customer " + str(message["customer_id"]))
         customer_websocket = CUSTOMERS.get(message["customer_id"])
-        print(customer_websocket)
-        if customer_websocket:
-            json_message = json.dumps({"scanned": True})
-            try:
-                await customer_websocket.send(json_message)
-            except websockets.ConnectionClosed:
-                pass
+
 
 async def main():
     try: 
